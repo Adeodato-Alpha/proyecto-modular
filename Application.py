0f3ef17343945ccc28ec6e6e8a8a4aebef3f6f0a
Adeodato <jesus2001adeodato@gmail.com>
2024-02-12 14:35:31 -0600

    añadir la wea esta

--- Application.py
+++ Application.py
@@ -1,3 +1,8 @@
 import serial as s 
 import tkinter as tk 
 import matplotlib.pyplot as plt  
+from time import sleep as sle 
+rp_port = s.Serial('COM3',115200) 
+while True:
+    print(rp_port.readline())
+    sle(0.1)
