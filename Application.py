4a8056ed6ed0888d20a76fe2aba5313cd99955bb
Adeodato <jesus2001adeodato@gmail.com>
2024-02-19 22:21:02 -0600

    avance GUI

--- Application.py
+++ Application.py
@@ -1,9 +1,48 @@
-import serial as s 
-import tkinter as tk 
-import matplotlib.pyplot as plt  
-from time import sleep as sle 
-rp_port = s.Serial('COM3',115200) 
-while True:
-    valores = rp_port.readline().decode('ascii').strip().split(',')
-    a1= [int(s) for s in valores]
-    sle(0.1)
+import serial, time , collections
+import matplotlib.pyplot as plt 
+import matplotlib.animation as anime 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import Tk, Frame, StringVar, Label, Button,Entry
+#Empezar la figura/Plot
+def animate(i, dataList, ser):                                    
+    arduinoData_string = ser.readline().decode('ascii').strip().split(',')
+    a1= [int(s) for s in arduinoData_string]                 
+
+    try:
+        arduinoData_float = float(a1[0])   
+        dataList.append(arduinoData_float)              
+
+    except:                                                                          
+        pass
+
+    dataList = dataList[-50:]                           
+    
+    ax.clear()                                          
+    ax.plot(dataList)                                  
+    
+    ax.set_ylim([0, 35000])                              
+    ax.set_title("RP2040")                        
+    ax.set_ylabel("Lectura")                              
+
+dataList = []                                           
+                                                        
+fig = plt.figure()                                      
+ax = fig.add_subplot(111)                               
+
+ser = serial.Serial('COM3',115200)                      
+time.sleep(2)                                           
+ani = anime.FuncAnimation(fig, animate, frames=100, fargs=(dataList, ser), interval=100) 
+
+                                            
+
+
+
+#Crear la GUI
+root = Tk()
+root.title("Modular Graphics ")
+
+
+grafic = FigureCanvasTkAgg(fig, master = root)
+grafic.get_tk_widget().pack(side='bottom', fill='both', expand=1)
+root.mainloop()
+ser.close()
